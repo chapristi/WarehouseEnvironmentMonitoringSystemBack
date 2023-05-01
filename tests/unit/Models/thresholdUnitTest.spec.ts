963f23111460eebb0e3fe213c604d8f120e5bf48
louis <louis@louis-IdeaPad-3-15IML05>
2023-05-01 21:05:12 +0200

    test the model threshold

--- tests/unit/Models/thresholdUnitTest.spec.ts
+++ tests/unit/Models/thresholdUnitTest.spec.ts
@@ -5,34 +5,38 @@ test('test threshold model true', async ({ assert }) => {
   const threshold : Threshold = new Threshold()
   threshold.fill({
      humidity : 15.5,
-     temperature : 25
+     temperature : 25,
+     sensorId : 1
      
   })
   assert.equal(15.5,threshold.humidity)
   assert.equal(25,threshold.temperature)
+  assert.equal(1,threshold.temperature)
 
 })
-/*
+
 test('test game entity is false', async ({ assert }) => {
 
-  const game : Game  = new Game()
-  game.fill({
-      longitude: 'false',
-      latitude: 'false', 
+  const threshold : Threshold  = new Threshold()
+  threshold.fill({
+    humidity : 15.5,
+    temperature : 25,
+    sensorId : 1
   })
-  assert.notEqual("true",game.longitude)
-  assert.notEqual("true",game.latitude)
+  assert.notEqual(32,threshold.humidity)
+  assert.notEqual(64,threshold.temperature)
+  assert.notEqual(2,threshold.sensorId)
  
 })
 
 test("test user entity is empty",async({assert})=>{
-  const game : Game  = new Game()
-  game.fill({
-      longitude: '',
-      latitude: '', 
-
+  const threshold : Threshold  = new Threshold()
+  threshold.fill({
+    humidity : undefined,
+    temperature : undefined,
+    sensorId: undefined
   })
-  assert.empty(game.longitude)
-  assert.empty(game.latitude)
+  assert.isUndefined(threshold.humidity)
+  assert.isUndefined(threshold.temperature)
+  assert.isUndefined(threshold.sensorId)
 })
-*/
